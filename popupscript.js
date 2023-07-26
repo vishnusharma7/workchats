7592003e05860161d429bd2ad0ae08fe204760db
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-27 03:15:49 +0530

    alll pop added

--- popupscript.js
+++ popupscript.js
@@ -36,7 +36,7 @@ cancelButton.addEventListener('click', () => {
 
 
 
-//your slack popup
+//your slack popup======================================================================================
 
 // Get the elements
 const yourSlackButton = document.getElementById('yourSlackButton');
@@ -76,45 +76,45 @@ signOutButton.addEventListener('click', function (event) {
 });
 
 
-// profile pop up script
+// profile pop up script===========================================================================
 
- // JavaScript
- const imgElement = document.querySelector('.profile-image-nav');
- const popupElement = document.querySelector('.profile-popup');
+// JavaScript
+const imgElement = document.querySelector('.profile-image-nav');
+const popupElement = document.querySelector('.profile-popup');
 
- // Function to show the pop-up
- function showPopups() {
-     popupElement.style.display = 'block';
- }
+// Function to show the pop-up
+function showPopups() {
+    popupElement.style.display = 'block';
+}
 
- // Function to hide the pop-up
- function hidePopups() {
-     popupElement.style.display = 'none';
- }
+// Function to hide the pop-up
+function hidePopups() {
+    popupElement.style.display = 'none';
+}
 
- imgElement.addEventListener('click', function (event) {
-     if (popupElement.style.display === 'block') {
-         hidePopups();
-     } else {
-         showPopups();
-     }
- });
+imgElement.addEventListener('click', function (event) {
+    if (popupElement.style.display === 'block') {
+        hidePopups();
+    } else {
+        showPopups();
+    }
+});
 
- // Event listener to hide the pop-up when clicking outside
- document.addEventListener('click', function (event) {
-     if (!imgElement.contains(event.target) && !popupElement.contains(event.target)) {
-         hidePopups();
-     }
- });
+// Event listener to hide the pop-up when clicking outside
+document.addEventListener('click', function (event) {
+    if (!imgElement.contains(event.target) && !popupElement.contains(event.target)) {
+        hidePopups();
+    }
+});
 
 
- //invite people
+//invite people===============================================================================================
 
- const inviteButton = document.querySelector('.invite')
- const invitePopup = document.querySelector('.invite-people')
+const inviteButton = document.querySelector('.invite')
+const invitePopup = document.querySelector('.invite-people')
 
-  // Function to show the pop-up
-  function inviteShowPopups() {
+// Function to show the pop-up
+function inviteShowPopups() {
     invitePopup.style.display = 'block';
 }
 
@@ -139,13 +139,13 @@ document.addEventListener('click', function (event) {
 });
 
 
-//create-channel-popup
+//create-channel-popup===============================================================================
 
 const createChanneButton = document.querySelector('.create-pop')
- const createChannelShow= document.querySelector('.create-channel-popup')
+const createChannelShow = document.querySelector('.create-channel-popup')
 
-  // Function to show the pop-up
-  function createShowPopups() {
+// Function to show the pop-up
+function createShowPopups() {
     createChannelShow.style.display = 'block';
 }
 
@@ -166,5 +166,130 @@ createChanneButton.addEventListener('click', function (event) {
 document.addEventListener('click', function (event) {
     if (!createChanneButton.contains(event.target) && !createChannelShow.contains(event.target)) {
         createHidePopups();
+    }
+});
+
+
+//setting-and-administration-popups=========================================================
+
+const settingChanneButton = document.querySelector('.setting-and-admin')
+const settingChannelShow = document.querySelector('.setting-adminitration-popup')
+
+// Function to show the pop-up
+function settingShowPopups() {
+    settingChannelShow.style.display = 'block';
+}
+
+// Function to hide the pop-up
+function settingHidePopups() {
+    settingChannelShow.style.display = 'none';
+}
+
+settingChanneButton.addEventListener('click', function (event) {
+    if (settingChannelShow.style.display === 'block') {
+        settingHidePopups();
+    } else {
+        settingShowPopups();
+    }
+});
+
+// Event listener to hide the pop-up when clicking outside
+document.addEventListener('click', function (event) {
+    if (!settingChanneButton.contains(event.target) && !settingChannelShow.contains(event.target)) {
+        settingHidePopups();
+    }
+});
+
+
+//add workspace pop-up==================================================================================
+
+const workButton = document.querySelector('.work-pop-up');
+const workShow = document.querySelector('.add-workspace-popup')
+
+// Function to show the pop-up
+function workShowPopups() {
+    workShow.style.display = 'block';
+}
+
+// Function to hide the pop-up
+function workHidePopups() {
+    workShow.style.display = 'none';
+}
+
+workButton.addEventListener('click', function (event) {
+    if (workShow.style.display === 'block') {
+        workHidePopups();
+    } else {
+        workShowPopups();
+    }
+});
+
+// Event listener to hide the pop-up when clicking outside
+document.addEventListener('click', function (event) {
+    if (!workButton.contains(event.target) && !workShow.contains(event.target)) {
+        workHidePopups();
+    }
+});
+
+//edit -workspace detail=========================================================
+
+const editWorkspaceButton = document.querySelector('.edit-workspace-button');
+const editWorkspaceShow = document.querySelector('.edit-workspace-detail-popup')
+const editCancelButton = document.querySelector('.button[data-action="editcancel"]');
+// Function to show the pop-up
+function editShowPopups() {
+    editWorkspaceShow.style.display = 'block';
+}
+
+// Function to hide the pop-up
+function editHidePopups() {
+    editWorkspaceShow.style.display = 'none';
+}
+
+editWorkspaceButton.addEventListener('click', function (event) {
+    if (editWorkspaceShow.style.display === 'block') {
+        editHidePopups();
+    } else {
+        editShowPopups();
+    }
+});
+
+// Event listener to hide the pop-up when clicking outside
+document.addEventListener('click', function (event) {
+    if (!editWorkspaceButton.contains(event.target) && !editWorkspaceShow.contains(event.target)) {
+        editHidePopups();
+    }
+});
+
+editCancelButton.addEventListener('click', () => {
+    editHidePopups();
+});
+
+
+//switch workspace popup=====================================================
+const switchButton = document.querySelector('.switch-button');
+const switchShow = document.querySelector('.switch-workspace-popup');
+// Function to show the pop-up
+function switchShowPopups() {
+    switchShow.style.display = 'block';
+}
+
+// Function to hide the pop-up
+function switchHidePopups() {
+    switchShow.style.display = 'none';
+}
+
+switchButton.addEventListener('click', function (event) {
+    if (switchShow.style.display === 'block') {
+        switchHidePopups();
+    } else {
+        switchShowPopups();
+    }
+});
+
+// Event listener to hide the pop-up when clicking outside
+document.addEventListener('click', function (event) {
+    if (!switchButton.contains(event.target) && !switchShow.contains(event.target)) {
+        switchHidePopups();
     }
 });
