1ca5b81d99c4451a833c0db749a29cd4fa9b8c05
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-29 16:55:13 +0530

    input box added

--- replythread.js
+++ replythread.js
@@ -1,21 +1,32 @@
 const replyonthread = document.querySelector(".reply");
-const threadBar = document.querySelector(".thread");
-let isMenuOpenthread = false;
+  const threadBar = document.querySelector(".thread");
+  const closeThread = document.querySelector(".close-thread");
+  let isMenuOpenthread = false;
 
-document.addEventListener("click", (event) => {
-  if (event.target === replyonthread) {
-    if (!isMenuOpenthread) {
-      threadBar.style.display = "block";
-      isMenuOpenthread = true;
-    } else {
+  document.addEventListener("click", (event) => {
+    if (event.target === replyonthread) {
+      if (!isMenuOpenthread) {
+        threadBar.style.display = "block";
+        isMenuOpenthread = true;
+      } else {
+        threadBar.style.display = "none";
+        isMenuOpenthread = false;
+      }
+    } else if (!threadBar.contains(event.target)) {
       threadBar.style.display = "none";
       isMenuOpenthread = false;
     }
-  } else if (!threadBar.contains(event.target)) {
+  });
+
+  closeThread.addEventListener("click", () => {
     threadBar.style.display = "none";
     isMenuOpenthread = false;
-  }
-});
-window.addEventListener("resize", handleViewportChange);
-window.addEventListener("DOMContentLoaded", handleViewportChange);
+  });
 
+  function handleViewportChange() {
+    // Implement your logic for handling viewport changes, if needed.
+    // This function is referenced in your code but not provided, so you may need to define it separately.
+  }
+
+  window.addEventListener("resize", handleViewportChange);
+  window.addEventListener("DOMContentLoaded", handleViewportChange);
