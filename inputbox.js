457ef6577b05d4560d7d9135ab3c0d9676587e7a
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-30 15:31:16 +0530

    changes UI

--- inputbox.js
+++ inputbox.js
@@ -1,19 +1,8 @@
-// const inputField = document.querySelector('.chatbox-msg');
-// const textStyles = document.querySelector('.input-msg');
-
-// inputField.addEventListener('click', () => {
-//     const svgs = textStyles.querySelectorAll('svg');
-//     svgs.forEach(svg => {
-//         svg.style.display = 'block';
-//     });
-// });
-
-
 const inputField = document.querySelector('.chatbox-msg');
 const textStyles = document.querySelector('.input-msg');
 
 inputField.addEventListener('click', (event) => {
-    const svgs = textStyles.querySelectorAll('svg');
+    const svgs = textStyles.querySelectorAll('svg:not(.rightbutton svg)');
     svgs.forEach(svg => {
         svg.style.display = 'block';
     });
@@ -24,7 +13,7 @@ inputField.addEventListener('click', (event) => {
 // Hiding the SVGs when clicking outside the inputField
 document.addEventListener('click', (event) => {
     if (!inputField.contains(event.target)) {
-        const svgs = textStyles.querySelectorAll('svg');
+        const svgs = textStyles.querySelectorAll('svg:not(.rightbutton svg)');
         svgs.forEach(svg => {
             svg.style.display = 'none';
         });
