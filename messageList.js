7f2e929581c5e52dc5985e2d3f1e1f1bf7abd1be
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-08-14 18:51:57 +0530

    bug fix

--- messageList.js
+++ messageList.js
@@ -1,10 +1,11 @@
 class MessageListComponent extends HTMLElement {
     constructor() {
       super();
-  
+     
       const template = document.createElement("template");
       template.innerHTML = `
-        <div id="messageContainer"></div>
+      <link rel="stylesheet" href="style.css">
+        <div class="column" id="messageContainer"></div>
       `;
   
       const shadowRoot = this.attachShadow({ mode: "open" });
@@ -17,6 +18,7 @@ class MessageListComponent extends HTMLElement {
       const messageContainer = this.shadowRoot.getElementById("messageContainer");
   
       const messages = [
+        { owner: "Alice", time: "6:51 PM", text: "First message from component received" },
         { owner: "Alice", time: "6:51 PM", text: "First message from component received" },
         { owner: "You", time: "6:51 PM", text: " message from sender" },
         { owner: "You", time: "7:05 PM", text: "Hello, this is a sender message" },
