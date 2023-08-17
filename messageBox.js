0c49168328ffd15d07b980911ae3e932ae095b83
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-08-18 00:10:04 +0530

    updated UI and added new web component

--- messageBox.js
+++ messageBox.js
@@ -1,3 +1,6 @@
+// ==================================receiver component======================================
+
+
 class MessageBoxComponent extends HTMLElement {
   constructor() {
     super();
@@ -126,4 +129,41 @@ customElements.define("message-box", MessageBoxComponent);
 
 
 
+// ==================================sender component======================================
+class SendMessageBoxComponent extends HTMLElement {
+    constructor() {
+      super();
+  
+      const template = document.createElement('template');
+      template.innerHTML = `
+        <link rel="stylesheet" href="style.css">
+        <div class="row sender">
+          <div class="column" style="gap: 0.3em;">
+            <div class="row sender-name-time">
+              <span class="message-owner"></span>
+              <span class="message-time"></span>
+            </div>
+            <span class="messagetext sender-text"></span>
+          </div>
+          <img class="img" src="sender.png" alt="">
+        </div>`;
+  
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+  
+    setSendMessageData(data) {
+      const shadowRoot = this.shadowRoot;
+      const ownerElement = shadowRoot.querySelector(".message-owner");
+      const timeElement = shadowRoot.querySelector(".message-time");
+      const textElement = shadowRoot.querySelector(".sender-text");
+  
+      ownerElement.textContent = data.owner || "";
+      timeElement.textContent = data.time || "";
+      textElement.textContent = data.text || "";
+    }
+  }
+  
+  customElements.define("send-messagebox", SendMessageBoxComponent);
 
+  
--- messageList.js
+++ messageList.js
@@ -1,47 +1,46 @@
 class MessageListComponent extends HTMLElement {
-    constructor() {
-      super();
-     
-      const template = document.createElement("template");
-      template.innerHTML = `
-      <link rel="stylesheet" href="style.css">
-        <div class="column" id="messageContainer"></div>
-      `;
-  
-      const shadowRoot = this.attachShadow({ mode: "open" });
-      shadowRoot.appendChild(template.content.cloneNode(true));
-  
-      this.renderMessages();
-    }
-  
-    renderMessages() {
-      const messageContainer = this.shadowRoot.getElementById("messageContainer");
-  
-      const messages = [
-        { owner: "Alice", time: "6:51 PM", text: "First message from component received" },
-        { owner: "Alice", time: "6:51 PM", text: "First message from component received" },
-        { owner: "You", time: "6:51 PM", text: " message from sender" },
-        { owner: "You", time: "7:05 PM", text: "Hello, this is a sender message" },
-        { owner: "Alice", time: "7:55 PM", text: "Reply to the first message" },
-        // Add more messages here
-      ];
-  
-      // Sort messages based on time
-      messages.sort((a, b) => new Date(a.time) - new Date(b.time));
-  
-      messages.forEach(messageData => {
-        if (messageData.owner === "You") {
-          const senderMessageBox = document.createElement("send-messagebox");
-          senderMessageBox.setSendMessageData(messageData);
-          messageContainer.appendChild(senderMessageBox);
-        } else {
-          const messageBox = document.createElement("message-box");
-          messageBox.setMessageData(messageData);
-          messageContainer.appendChild(messageBox);
-        }
-      });
-    }
+  constructor() {
+    super();
+   
+    const template = document.createElement("template");
+    template.innerHTML = `
+    <link rel="stylesheet" href="style.css">
+      <div class="column" id="messageContainer"></div>
+    `;
+
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.renderMessages();
   }
-  
-  customElements.define("message-list", MessageListComponent);
-  
+
+  renderMessages() {
+    const messageContainer = this.shadowRoot.getElementById("messageContainer");
+
+    const messages = [
+      { owner: "Alice", time: "6:51 PM", text: "First message from component received" },
+      { owner: "Alice", time: "6:51 PM", text: "First message from component received" },
+      { owner: "You", time: "6:51 PM", text: " message from sender" },
+      { owner: "You", time: "7:05 PM", text: "Hello, this is a sender message" },
+      { owner: "Alice", time: "7:55 PM", text: "Reply to the first message" },
+      // Add more messages here
+    ];
+
+    // Sort messages based on time
+    messages.sort((a, b) => new Date(a.time) - new Date(b.time));
+
+    messages.forEach(messageData => {
+      if (messageData.owner === "You") {
+        const senderMessageBox = document.createElement("send-messagebox");
+        senderMessageBox.setSendMessageData(messageData);
+        messageContainer.appendChild(senderMessageBox);
+      } else {
+        const messageBox = document.createElement("message-box");
+        messageBox.setMessageData(messageData);
+        messageContainer.appendChild(messageBox);
+      }
+    });
+  }
+}
+
+customElements.define("message-list", MessageListComponent);
