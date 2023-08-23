8347b2de1d9481a717e61de2e9a10de50db49527
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-08-23 14:40:55 +0530

    9+ feature added

--- replythread.js
+++ replythread.js
@@ -1,33 +1,33 @@
-  const replyonthread = document.querySelector(".reply");
-  const threadBar = document.querySelector(".thread");
-  const closeThread = document.querySelector(".close-thread");
-  let isMenuOpenthread = false;
+  // const replyonthread = document.querySelector(".reply");
+  // const threadBar = document.querySelector(".thread");
+  // const closeThread = document.querySelector(".close-thread");
+  // let isMenuOpenthread = false;
 
-  document.addEventListener("click", (event) => {
-    if (event.target === replyonthread) { 
-      if (!isMenuOpenthread) {
-        threadBar.style.display = "flex";
-        isMenuOpenthread = true;
-      } else {
-        threadBar.style.display = "none";
-        isMenuOpenthread = false;
-      }
-    } else if (!threadBar.contains(event.target)) {
-      threadBar.style.display = "none";
-      isMenuOpenthread = false;
-    }
-  });
+  // document.addEventListener("click", (event) => {
+  //   if (event.target === replyonthread) { 
+  //     if (!isMenuOpenthread) {
+  //       threadBar.style.display = "flex";
+  //       isMenuOpenthread = true;
+  //     } else {
+  //       threadBar.style.display = "none";
+  //       isMenuOpenthread = false;
+  //     }
+  //   } else if (!threadBar.contains(event.target)) {
+  //     threadBar.style.display = "none";
+  //     isMenuOpenthread = false;
+  //   }
+  // });
 
-  closeThread.addEventListener("click", () => {
-    threadBar.style.display = "none";
-    isMenuOpenthread = false;
-  });
+  // closeThread.addEventListener("click", () => {
+  //   threadBar.style.display = "none";
+  //   isMenuOpenthread = false;
+  // });
 
-  function handleViewportChange() {
+  // function handleViewportChange() {
     
-  }
+  // }
 
-  window.addEventListener("resize", handleViewportChange);
-  window.addEventListener("DOMContentLoaded", handleViewportChange);
+  // window.addEventListener("resize", handleViewportChange);
+  // window.addEventListener("DOMContentLoaded", handleViewportChange);
 
   
--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 // Function to handle the file selection
-function handleFileSelect(event) 
-  {
-    const file = event.target.files[0]; // Get the selected file
-  
-    if (file) {
-      const reader = new FileReader();
-  
-      reader.onload = function (e) {
-        const profileImage = document.getElementById('profile-image');
-        profileImage.src = e.target.result; // Set the source of the image to the selected file
-      };
-  
-      reader.readAsDataURL(file); // Read the file as a data URL
-    }
-  }
-  
-  // Add event listener to the "Change" button
-  const changeImageButton = document.getElementById('change-image-button');
-  changeImageButton.addEventListener('click', function () {
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.accept = 'image/*';
-    fileInput.addEventListener('change', handleFileSelect);
-    fileInput.click(); // Simulate a click on the file input element
-  });
 
 
 
